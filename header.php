51daa38d0f2fee8b376ec0dd42ad5f6e28e0f856
ggeugene <g.eugene.g@outlook.com>
2019-04-09 01:00:31 +0300

    Added dummy header and footer.

---
+++ header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+
+
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <link rel="icon" href="/wp-content/themes/itop/img/healthpartner-favicon.png" type="image/x-icon">
+	<?php wp_head(); ?>
+</head>
+
+<body>
+    <header>
+        <div class="top-bar" id="top-bar">
+            <div class="container">
+                <div class="row d-none d-lg-flex justfify-content-between">
+                    <div class="col-4">
+                        <div class="social-container">Socials</div>
+                    </div>
+                    <div class="col-4">
+                        <div class="logo-container">
+                            <a href="/" class="logo-container__link">
+                                <img class="logo-container__image" src="/wp-content/themes/itop/img/healtpartner-logo.png" alt="healthpartner-logo">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="workhours-container">Working<br/> hours</div>
+                            </div>
+                            <div class="col-6">
+                                <div class="phones-container">phone1<br/> phone2</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header" id="main-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <ul class="d-flex justify-content-between">
+                            <li>Menu item</li>
+                            <li>Menu item</li>
+                            <li>Menu item</li>
+                            <li>Menu item</li>
+                            <li>Menu item</li>
+                            <li>Menu item</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
